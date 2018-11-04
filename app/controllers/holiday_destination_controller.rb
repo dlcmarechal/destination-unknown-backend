e27b2f08b422e11a99d7f57e1f4bd6ba82862f2f
Joey Kaan <jkaan@users.noreply.github.com>
2018-11-04 14:34:16 +0100

    Integrate python script

--- app/controllers/holiday_destination_controller.rb
+++ app/controllers/holiday_destination_controller.rb
@@ -2,7 +2,7 @@ require 'open3'
 
 class HolidayDestinationController < ApplicationController
   def generate
-    stdout, _ = Open3.capture2("python output-suggestions.py")
-    render json: stdout
+    stdout, _ = Open3.capture2("python holiday-destination/output-suggestions.py")
+    render json: {"country" => stdout.chomp }
   end
 end
