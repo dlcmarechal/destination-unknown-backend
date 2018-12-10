0d5d1c8ddc676f7b745b0b9611e3b0f227549bf0
Joey Kaan <jkaan@users.noreply.github.com>
2018-12-10 17:43:06 +0100

    Add CORS allow

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module HolidayDestinationBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get]
+        resource '*', headers: :any, methods: [:get, :post]
       end
     end
 
